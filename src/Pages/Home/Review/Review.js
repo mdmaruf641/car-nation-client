0639eff9503ce80501f8b4246f07d52b5ca26f07
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-11-13 21:04:27 +0600

    rating added

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Review.css";
 import user from "../../../images/user.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Rating from "react-rating";
 
 const Review = (props) => {
   const { name, comment, rating } = props.review;
+  console.log(parseFloat(rating));
+  const star = <FontAwesomeIcon icon={faStar} />;
   return (
     <div>
       <div className="col">
@@ -11,7 +16,14 @@ const Review = (props) => {
           <img src={user} className="card-img-top w-25 mx-auto mt-3" alt="" />
           <div className="card-body">
             <h3 className="card-title">{name}</h3>
-            <p className="card-text">rating</p>
+            <p className="card-text">
+              <Rating
+                initialRating={rating}
+                emptySymbol="far fa-star icon-color"
+                fullSymbol="fas fa-star icon-color"
+                readonly
+              ></Rating>
+            </p>
             <h6 className="fst-italic">" {comment} "</h6>
           </div>
         </div>
